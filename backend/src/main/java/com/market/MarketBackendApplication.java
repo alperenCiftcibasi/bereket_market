eaaf7afc834f4a0075667fc0042e3897aee40db1
mehmet bal <kalekasabas@gmail.com>
2025-06-23 00:14:18 +0300

    Swagger UI ile API test edildi ,ilerde fronted alanı yapıldığında ayrıca test edebiliriz.

--- backend/src/main/java/com/market/MarketBackendApplication.java
+++ backend/src/main/java/com/market/MarketBackendApplication.java
@@ -3,6 +3,13 @@ package com.market;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+	    info = @Info(title = "Market API", version = "1.0", description = "Market uygulaması API dökümantasyonu")
+	)
+
 @SpringBootApplication
 public class MarketBackendApplication {
     public static void main(String[] args) {
